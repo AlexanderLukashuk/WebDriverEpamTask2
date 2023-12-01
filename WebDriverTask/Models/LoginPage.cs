782b789810a588f45ab7becc5ab541ea87915cd0
AlexanderLukashuk <upiter465@gmail.com>
2023-12-01 21:36:14 +0600

    LoginPage class created

--- WebDriverTask/Models/LoginPage.cs
+++ WebDriverTask/Models/LoginPage.cs
@@ -2,11 +2,46 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using OpenQA.Selenium;
 
 namespace WebDriverTask.Models
 {
     public class LoginPage
     {
-        
+        private IWebDriver webDriver;
+
+        public LoginPage(IWebDriver driver)
+        {
+            webDriver = driver;
+        }
+
+        public void NavigateToLoginPage(string url)
+        {
+            webDriver.Navigate().GoToUrl(url);
+        }
+
+        public void Login(string username, string password)
+        {
+            var usernameInput = webDriver.FindElement(By.Id("username"));
+            var passwordInput = webDriver.FindElement(By.Id("password"));
+            var loginButton = webDriver.FindElement(By.CssSelector("button[type='submit']"));
+
+            usernameInput.SendKeys(username);
+            passwordInput.SendKeys(password);
+            loginButton.Click();
+        }
+
+        public bool IsLoginSuccessful()
+        {
+            try
+            {
+                var successElement = webDriver.FindElement(By.CssSelector(".success-message"));
+                return successElement != null;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
     }
 }
